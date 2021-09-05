b5797cc378582597ddee6e60b23f6ab17c5c552f
aya10969 <shawty061@gmail.com>
2021-09-05 15:45:06 +0900

    出品ページへの移行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+
+ with_options presence: true do
+   validates :product_name
+   validates :product_price
+   validates :explanation  
+   validates :category_id 
+   validates :product_condition_id 
+   validates :shipping_charge_id  
+   validates :prefecture_id 
+   validates :ays_to_ship_id  
+ end
+
 end
