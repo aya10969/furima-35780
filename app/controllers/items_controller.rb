0f4d76e36ad569610d04d65b31bd88f798718bf5
aya10969 <shawty061@gmail.com>
2021-09-24 07:15:23 +0900

    エラーハンドリングの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [ :show, :edit]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [ :show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -24,6 +24,15 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+     else
+     render :edit
+     end
+  end
+
 
   private
 
