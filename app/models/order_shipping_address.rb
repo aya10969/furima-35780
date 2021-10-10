a884fa135cdce55f8e09d63e46abe7fdc32b732a
aya10969 <shawty061@gmail.com>
2021-10-10 19:34:42 +0900

    配送先の単体テストコード

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -5,17 +5,15 @@ class OrderShippingAddress
   with_options presence: true do
     validates :municipality
     validates :address
-    validates :user_id
   end
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-    validates :telephone_number, numericality:{ only_integer: true }, allow_blank: true
-    validates :building_name, presence: true, allow_blank: true
+    validates :postal_code, presence: true,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, presence: true,numericality: {other_than: 0, message: "can't be blank"}
+    validates :telephone_number, presence: true,numericality:{ only_integer: true }
    
     def save
       order = Order.create(item_id: item_id, user_id: user_id)
       # 住所を保存する
-      # donation_idには、変数donationのidと指定する
+      # shipping_addres_idには、変数shipping_addresのidと指定する
       ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, telephone_number: telephone_number, order_id: order.id)
     end
 end
