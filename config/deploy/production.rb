c6212fd7af68e7f3e49a4e3ab2db13c833605268
aya10969 <shawty061@gmail.com>
2021-10-24 14:50:02 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -49,7 +49,7 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-#server "13.113.201.114",user: "user_name",roles: %w{app db web},
+server "13.113.201.114",user: "ec2-user",roles: %w{app db web},
 #   ssh_options: {
 #     user: "user_name", # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
